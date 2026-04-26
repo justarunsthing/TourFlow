71d3a340474e203e20a65f51b8223f4eea07c383
arunpun <83875939+arunpun@users.noreply.github.com>
2026-04-26 18:48:33 +0100

    Added EnumExtensions

---
+++ TourFlow.Client/Extensions/EnumExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.ComponentModel.DataAnnotations;
+
+namespace TourFlow.Client.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum value)
+        {
+            string? displayName = value.GetType()
+                .GetMember(value.ToString())
+                .FirstOrDefault()?
+                .GetCustomAttribute<DisplayAttribute>()?
+                .GetName();
+
+            if (string.IsNullOrEmpty(displayName))
+            {
+                return value.ToString();
+            }
+            else
+            {
+                return displayName;
+            }
+        }
+    }
+}
